7f5bfbdeffc26ab2232dd33dd8bf4b1111e700f7
venkat <bhvreddy88@gmail.com>
2017-11-03 17:49:18 +0530

    default float to left - user can change by using float attribute - left or right
    in image user can select - but while wrapping shorcode content changes when change the allign -
    so its better to add float using attributes

--- inc/class-csh-shortcode.php
+++ inc/class-csh-shortcode.php
@@ -4,25 +4,36 @@
  * 
  */
 
-class CSH_Shortcode {
+ class CSH_Shortcode {
 
     public function shortcode( $atts = [], $content = 'null', $shortcode = '') {
 
         $a = shortcode_atts(
             array(
                 'id' => 'default-one',
+                'float' => 'left',
             ), $atts, $shortcode );
            // use like -  '.$a["title"].'   
         
         $id = $a["id"];
+        $float = $a["float"];
+
+        if( 'left' == $float) {
+            $float_class = 'csh-left';
+        } elseif ( 'right' == $float ) {
+            $float_class = 'csh-right';
+        }
 
         $url =  wp_get_attachment_url( $id );
 
         $o = '';
-        $o .= '<div class="csh-left csh-margin" style="shape-outside: url('.$url.'); " >';
+        $o .= '<div class=" '.$float_class.' csh-margin" style="shape-outside: url('.$url.'); " >';
         $o .= $content;
         $o .= '</div >';
         
+        // $i = wp_get_attachment_image( $id );
+        // return $i;
+
         return $o;
     }
 
--- inc/test.php
+++ inc/test.php
@@ -5,16 +5,14 @@
 
 $x = wp_get_attachment_url( 57 );
 
-$y = wp_get_attachment_link( 42 );
+$y = wp_get_attachment_image( 57 );
 
 
+$z = wp_get_attachment_link( 42 );
 
-// $images = get_children( 'post_type=attachment&post_mime_type=image' );
+$images = get_children( 'post_type=attachment&post_mime_type=image' );
 
-// if ( empty($images) ) {
-// 	// no attachments here
-// } else {
-// 	foreach ( $images as $attachment_id => $attachment ) {
-// 		echo wp_get_attachment_link( $attachment_id, 'full' );
-// 	}
-// }
+// echo '<pre>';
+// var_dump( $x );
+// echo '</pre>';
+

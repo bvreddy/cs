991da2d897fabb454fa2e6fbb7fae218c2d31f90
venkat <bhvreddy88@gmail.com>
2017-11-04 14:00:47 +0530

    added shortcode atts - margin , float ...
    added another shortcode - img - planning on.. to add image using shortcode  - using id ..

--- inc/class-csh-shortcode.php
+++ inc/class-csh-shortcode.php
@@ -6,18 +6,26 @@
 
  class CSH_Shortcode {
 
+    // for svg, transparent images it good.. 
     public function shortcode( $atts = [], $content = 'null', $shortcode = '') {
 
+        /**
+         * 
+         * @param id - image id
+         * @param shape-outside -  shape-outside value
+         */
         $a = shortcode_atts(
             array(
-                'id' => 'default-one',
+                'id' => '',
                 'float' => 'left',
+                'margin' => '1em',
             ), $atts, $shortcode );
            // use like -  '.$a["title"].'   
         
         $id = $a["id"];
         $float = $a["float"];
-
+        $margin = $a["margin"];
+        
         if( 'left' == $float) {
             $float_class = 'csh-left';
         } elseif ( 'right' == $float ) {
@@ -25,11 +33,60 @@
         }
 
         $url =  wp_get_attachment_url( $id );
+        $shape_outside = "url($url)";
 
+        
         $o = '';
-        $o .= '<div class=" '.$float_class.' csh-margin" style="shape-outside: url('.$url.'); " >';
+        $o .= '<span class=" '.$float_class.' " style="shape-outside: '.$shape_outside.'; shape-margin: '.$margin.' " >';
         $o .= $content;
-        $o .= '</div >';
+        $o .= '</span>';
+        
+        return $o;
+    }
+
+
+
+
+
+
+    // directly adding images..
+    public function img( $atts = [], $content = 'null', $shortcode = '') {
+        
+        $a = shortcode_atts(
+            array(
+                'id' => '',
+                'float' => 'left',
+                'shape' => '',
+            ), $atts, $shortcode );
+            // use like -  '.$a["title"].'   
+        
+        $id = $a["id"];
+        $float = $a["float"];
+        $shape = $a["shape"];
+        
+
+        if( 'left' == $float) {
+            $float_class = 'csh-left';
+        } elseif ( 'right' == $float ) {
+            $float_class = 'csh-right';
+        }
+
+        if( null == $shape || '' == $shape ) {
+            $url =  wp_get_attachment_url( $id );
+            $shape_outside = "url($url)";
+        } else {
+            $shape_outside = $shape;
+        }
+
+        
+
+
+
+        // polygon(39% 0%, 87% 72%, 86% 99%, 100% 100%, 100% 0%);
+        
+        $o = '';
+        $o .= '<img src=" '.$url.' ">';
+        $o .= '';
         
         // $i = wp_get_attachment_image( $id );
         // return $i;
@@ -38,9 +95,15 @@
     }
 
 
+
+
+
+
+
     //  Register shortcode
     public function csh_shortcodes_init() {
         add_shortcode('shape', array( $this, 'shortcode' ) );
+        add_shortcode('img', array( $this, 'img' ) );
     }
 
 }

64b9032fe7784d3d05936d6f7c283e3d4be958d2
venkat <bhvreddy88@gmail.com>
2017-11-05 09:21:53 +0530

    shape - shortcode -  shape can given using url or image id ..

--- inc/class-csh-shortcode.php
+++ inc/class-csh-shortcode.php
@@ -13,27 +13,38 @@
          * 
          * @param id - image id
          * @param shape-outside -  shape-outside value
+         * @param url  - image url - shape can be given using image id or direct url
+         * 
+         * @var url_id  - get image url based on id
          */
         $a = shortcode_atts(
             array(
                 'id' => '',
+                'url' => '',
                 'float' => 'left',
                 'margin' => '1em',
             ), $atts, $shortcode );
            // use like -  '.$a["title"].'   
         
-        $id = $a["id"];
+        $img_id = $a["id"];
         $float = $a["float"];
         $margin = $a["margin"];
+        $url = $a["url"];
         
+        $url_id =  wp_get_attachment_url( $img_id );
+
+        if ( '' == $url || null == $url ) {
+            $shape_outside = "url($url_id)";
+        } else {
+            $shape_outside = "url($url)";
+        }
+
         if( 'left' == $float) {
             $float_class = 'csh-left';
         } elseif ( 'right' == $float ) {
             $float_class = 'csh-right';
         }
 
-        $url =  wp_get_attachment_url( $id );
-        $shape_outside = "url($url)";
 
         
         $o = '';
@@ -55,22 +66,23 @@
         $a = shortcode_atts(
             array(
                 'id' => '',
+                'url' => '',
                 'float' => 'left',
-                'shape' => '',
+                'shape-outside' => '',
                 'margin' => '1em',
                 'width' => null,
                 'height' => null,
             ), $atts, $shortcode );
             // use like -  '.$a["title"].'   
         
-        $id = $a["id"];
+        $img_id = $a["id"];
         $float = $a["float"];
-        $shape = $a["shape"];
+        $shape = $a["shape-outside"];
         $margin = $a["margin"];
         $width = $a["width"];
         $height = $a["height"];
         
-        $url =  wp_get_attachment_url( $id );
+        $url_id =  wp_get_attachment_url( $img_id );
 
         if( 'left' == $float) {
             $float_class = 'csh-left';
@@ -78,20 +90,25 @@
             $float_class = 'csh-right';
         }
 
-
-        if( null == $shape || '' == $shape ) {
-            $shape_outside = "url($url)";
+        if ( '' == $url || null == $url ) {
+            $shape_outside = "url($url_id)";
         } else {
-            $shape_outside = $shape;
+            $shape_outside = "url($url)";
         }
+        
+        // if( null == $shape || '' == $shape ) {
+        //     $shape_outside = "url($url_id)";
+        // } else {
+        //     $shape_outside = $shape;
+        // }
 
         
 
         // polygon(39% 0%, 87% 72%, 86% 99%, 100% 100%, 100% 0%);
         
-        $o = '';
-        $o .= '<img src=" '.$url.' ">';
-        $o .= '';
+        // $o = '';
+        // $o .= '<img src=" '.$url_id.' ">';
+        // $o .= '';
         
 
         // there is a problem if pass this type - with suffix for - svg images ..
@@ -107,8 +124,8 @@
 
 
         $i = '';
-        // $i .= ' '. wp_get_attachment_image( $id, $array_size, '', $array_attr ).' ';
-        $i .= ' '. wp_get_attachment_image( $id, '', '', $array_attr ).' '; 
+        // $i .= ' '. wp_get_attachment_image( $img_id, $array_size, '', $array_attr ).' ';
+        $i .= ' '. wp_get_attachment_image( $img_id, '', '', $array_attr ).' '; 
         $i .= '';
         $i .= '';
 

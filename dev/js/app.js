f4dec32fa32ff45e55552657371d151391e67ae6
venkat <bhvreddy88@gmail.com>
2017-11-07 15:45:03 +0530

    shortcode - button in tinymce editor - works fine ..

--- dev/js/app.js
+++ dev/js/app.js
@@ -6,21 +6,66 @@
 
 
 
-(function() {
-    tinymce.create('tinymce.plugins.quote', {
-        init : function(ed, url) {
-            ed.addButton('quote', {
-                title : 'Add a Quote',
-                image : url+'/image.png',
-                onclick : function() {
-                     ed.selection.setContent('[quote]' + ed.selection.getContent() + '[/quote]');
+// (function() {
+//     tinymce.create('tinymce.plugins.quote', {
+//         init : function(ed, url) {
+//             ed.addButton('quote', {
+//                 title : 'Add a Quote',
+//                 image : url+'/image.png',
+//                 onclick : function() {
+//                      ed.selection.setContent('[quote]' + ed.selection.getContent() + '[/quote]');
  
+//                 }
+//             });
+//         },
+//         createControl : function(n, cm) {
+//             return null;
+//         },
+//     });
+//     tinymce.PluginManager.add('quote', tinymce.plugins.quote);
+// })();
+
+
+
+(function() {
+    tinymce.create('tinymce.plugins.recentposts', {
+       init : function(ed, url) {
+          ed.addButton('recentposts', {
+             title : 'Recent posts',
+             image : url+'/recentpostsbutton.png',
+             onclick : function() {
+                var posts = prompt("Number of posts", "1");
+                var text = prompt("List Heading", "This is the heading text");
+ 
+                if (text != null && text != ''){
+                   if (posts != null && posts != '')
+                      ed.execCommand('mceInsertContent', false, '[recent-posts posts="'+posts+'"]'+text+'[/recent-posts]');
+                   else
+                      ed.execCommand('mceInsertContent', false, '[recent-posts]'+text+'[/recent-posts]');
                 }
-            });
-        },
-        createControl : function(n, cm) {
-            return null;
-        },
+                else{
+                   if (posts != null && posts != '')
+                      ed.execCommand('mceInsertContent', false, '[recent-posts posts="'+posts+'"]');
+                   else
+                      ed.execCommand('mceInsertContent', false, '[recent-posts]');
+                }
+             }
+          });
+       },
+       createControl : function(n, cm) {
+          return null;
+       },
+       getInfo : function() {
+          return {
+             longname : "Recent Posts",
+             author : 'Konstantinos Kouratoras',
+             authorurl : 'http://www.kouratoras.gr',
+             infourl : '',
+             version : "1.0"
+          };
+       }
     });
-    tinymce.PluginManager.add('quote', tinymce.plugins.quote);
-})();
+    tinymce.PluginManager.add('recentposts', tinymce.plugins.recentposts);
+ })();
+
+ 

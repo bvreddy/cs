fa73dec2a9c6689d8aa8d87e03115a374c2250d5
venkat <bhvreddy88@gmail.com>
2017-12-01 12:40:52 +0530

    ..

--- inc/test.php
+++ inc/test.php
@@ -3,7 +3,7 @@ echo 'works - test.php';
 ?>
 
 
-<!-- <script>
+<script>
 console.log('log message .. ');
   window.fbAsyncInit = function() {
     FB.init({
@@ -27,10 +27,14 @@ console.log('log message .. ');
 
 <div class="fb-customerchat"
  page_id="1744915998866434"
- ref="<OPTIONAL_WEBHOOK_PARAM>"
+ ref=""
  minimized="true">
-</div> -->
+</div>
 
-<script>  
+
+
+<!-- <script>  
 console.log('chatfuel .........');
-    window.fbMessengerPlugins = window.fbMessengerPlugins || {        init: function () {          FB.init({            appId            : '1678638095724206',            autoLogAppEvents : true,            xfbml            : true,            version          : 'v2.10'          });        }, callable: []      };      window.fbAsyncInit = window.fbAsyncInit || function () {        window.fbMessengerPlugins.callable.forEach(function (item) { item(); });        window.fbMessengerPlugins.init();      };      setTimeout(function () {        (function (d, s, id) {          var js, fjs = d.getElementsByTagName(s)[0];          if (d.getElementById(id)) { return; }          js = d.createElement(s);          js.id = id;          js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";          fjs.parentNode.insertBefore(js, fjs);        }(document, 'script', 'facebook-jssdk'));      }, 0);      </script>            <div        class="fb-customerchat"        page_id="1744915998866434"        ref="">      </div>    
+    window.fbMessengerPlugins = window.fbMessengerPlugins || {        init: function () {          FB.init({            appId            : '1678638095724206',            autoLogAppEvents : true,            xfbml            : true,            version          : 'v2.10'          });        }, callable: []      };      window.fbAsyncInit = window.fbAsyncInit || function () {        window.fbMessengerPlugins.callable.forEach(function (item) { item(); });        window.fbMessengerPlugins.init();      };      setTimeout(function () {        (function (d, s, id) {          var js, fjs = d.getElementsByTagName(s)[0];          if (d.getElementById(id)) { return; }          js = d.createElement(s);          js.id = id;          js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";          fjs.parentNode.insertBefore(js, fjs);        }(document, 'script', 'facebook-jssdk'));      }, 0);      </script>           
+    
+ <div        class="fb-customerchat"        page_id="1744915998866434"        ref="">      </div>     -->
